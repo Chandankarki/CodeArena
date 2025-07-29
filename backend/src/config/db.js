9398a2715ce314833da59a430b67a2163a6c0933
Chandan Karki <kchandan2205@gmail.com>
2025-07-29 19:22:40 +0530

    Refactor: Remove commented-out code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose');
-// async function main() {
-//     await mongoose.connect(process.env.DB_CONNECT_STRING);
-// }
-
-// module.exports = main;
-
-
-// const mongoose = require('mongoose');
-
-// async function main() {
-//     await mongoose.connect(process.env.DB_CONNECT_STRING, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         tls: true
-//     });
-//     console.log("✅ MongoDB connected");
-// }
-
-// module.exports = main;
-
 
 
 const mongoose = require('mongoose');
--- backend/src/routes/submit.js
+++ backend/src/routes/submit.js
@@ -1,31 +1,4 @@
 
-// const express = require('express');
-// const submitRouter = express.Router();
-// const userMiddleware = require('../middleware/userMiddleware');
-// const {submitCode, runCode} = require('../controllers/userSubmission');
-
-// submitRouter.post("/submit/:id", userMiddleware, submitCode);
-// submitRouter.post("/run/:id", userMiddleware, runCode);
-
-// module.exports = submitRouter;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const submitRouter = express.Router();
 const userMiddleware = require("../middleware/userMiddleware");
